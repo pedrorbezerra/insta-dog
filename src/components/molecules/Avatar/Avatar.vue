d1e318a2836b5d1f3bd4f780f62b68cc4f8310f7
pedro.bezerra@olistpax.com <pedro.bezerra@olistpax.com>
2022-02-17 21:12:13 -0300

    create avatar component

---
+++ src/components/molecules/Avatar/Avatar.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="Profile__avatar is-flex is-justify-content-center is-align-items-center is-flex-direction-column mt-4">
+        <figure class="image is-128x128">
+            <b-image
+                :src="image"
+                :src-fallback="fallbackImage"
+                ratio="1by1"
+                rounded
+            ></b-image>
+        </figure>
+        <p class="title is-3 mt-4">{{ name }}</p>
+    </div>
+</template>
+
+<script>
+export default ({
+    name: 'Avatar',
+
+    props: {
+        name: {
+            type: String,
+            default: ''
+        },
+        
+        image: {
+            type: String,
+            default: '',
+            required: true 
+        }
+    },
+
+    data() {
+        return {
+            fallbackImage: 'https://yt3.ggpht.com/ytc/AKedOLQf5MBcFSDzo2FeZIXSqafCvdRMGjW2C-0j8RpD=s900-c-k-c0x00ffffff-no-rj'
+        }
+    }
+})
+</script>
---
+++ src/views/Profile/Profile.vue
@@ -0,0 +1,16 @@
+<template>
+    <section class="Profile container">
+        <Avatar name="Tereza da Silva Sauro" image="https://uploads.metropoles.com/wp-content/uploads/2021/06/09110407/cachorro-fofo-usando-oculos_23-2148917262-1-600x380.jpg"/>
+    </section>
+</template>
+
+<script>
+import Avatar from '@/components/molecules/Avatar/Avatar'
+
+export default ({
+    name: 'Profile',
+    components: {
+        Avatar
+    }
+})
+</script>
