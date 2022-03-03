2c4068b085dfc83ae34166bfa08fa938d6323ff2
pedrorodriguesbezerra <31513148+pedrorodriguesbezerra@users.noreply.github.com>
2022-03-02 22:33:49 -0300

    add new images into profile

--- src/views/Profile/Profile.vue
+++ src/views/Profile/Profile.vue
@@ -9,6 +9,32 @@
 
             <b-button class="Profile__editButton" @click="editProfile">Editar Perfil</b-button>
         </div>
+        
+
+        <div class="is-flex container is-flex-wrap-wrap	is-justify-content-center">
+            <figure v-for="photo in photos" 
+                :key="photo.id" class="image Profile__photo" 
+                @mouseover="handlerPhotoHover(true, photo.id)" 
+                @mouseleave="handlerPhotoHover(false, photo.id)"
+                @click="openImage(photo.path)"
+            >
+                <b-image
+                    :src="photo.path"
+                    ratio="1by1"
+                ></b-image>
+
+                <div v-if="photoHover[photo.id]" class="Profile__backdrop">
+                   <div class="Profile__action-icons">
+                       <span><b-icon pack="fas" icon="heart" type="is-white" /> {{ photo.likes }}</span>
+                       <span><b-icon pack="fas" icon="comment" type="is-white" /> {{ photo.comments }}</span>
+                   </div>
+                </div>
+            </figure>
+        </div>
+
+        <b-modal v-model="photo">
+            <div :style="`background-image:url('${photo}')`" class="Profile__modal" />
+        </b-modal>
     </section>
 </template>
 
@@ -22,7 +48,60 @@ export default ({
     },
 
     data() {
-        return {}
+        return {
+            photos: [
+                {
+                    id: 0,
+                    path: require('@/assets/images/1.png'),
+                    likes: 9,
+                    comments: 2
+                },
+                {
+                    id: 1,
+                    path: require('@/assets/images/2.png'),
+                    likes: 5,
+                    comments: 7
+                },
+                {
+                    id: 2,
+                    path: require('@/assets/images/3.png'),
+                    likes: 1,
+                    comments: 21
+                },
+                {
+                    id: 3,
+                    path: require('@/assets/images/4.png'),
+                    likes: 9,
+                    comments: 2
+                },
+                {
+                    id: 4,
+                    path: require('@/assets/images/5.png'),
+                    likes: 1,
+                    comments: 21
+                },
+                {
+                    id: 5,
+                    path: require('@/assets/images/6.png'),
+                    likes: 5,
+                    comments: 7
+                },
+                {
+                    id: 6,
+                    path: require('@/assets/images/7.png'),
+                    likes: 51,
+                    comments: 73
+                },
+                {
+                    id: 7,
+                    path: require('@/assets/images/8.png'),
+                    likes: 25,
+                    comments: 17
+                }
+            ],
+            photoHover: {},
+            photo: null
+        }
     },
 
     computed: {
@@ -34,6 +113,15 @@ export default ({
     methods: {
         editProfile() {
             this.$router.push('profile/edit')
+        },
+
+        handlerPhotoHover(event, id) {
+            this.$set(this.photoHover, id, false)
+            this.photoHover[id] = event
+        },
+
+        openImage(image) {
+            this.photo = image
         }
     }
 })
@@ -45,4 +133,37 @@ export default ({
         padding: 10px
         min-width: 100px
         text-align: center
+
+    &__photo
+        width: 293px
+        height: 293px
+        margin: 15px
+        position: relative
+        cursor: pointer
+
+    &__backdrop
+        width: 293px
+        height: 293px
+        position: absolute
+        top: 0
+        left: 0
+        background: rgba(0, 0, 0, 0.3)
+    
+    &__action-icons
+        height: 100%
+        display: flex
+        align-items: center
+        justify-content: center
+
+        > span 
+            color: white
+            display: flex
+            justify-content: space-around
+            margin: 0 10px
+
+    &__modal
+        height: 85vh
+        background-size: cover
+        object-fit: cover
+        background-position: center
 </style>
